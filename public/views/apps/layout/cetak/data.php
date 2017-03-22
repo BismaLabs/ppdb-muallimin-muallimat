ec370a9dd3fedd2a18528c38d8e44c7a7daad976
maulayyacyber <maulayyacyber@gmail.com>
2017-03-22 20:57:28 +0700

    fix style cetak

--- public/views/apps/layout/cetak/data.php
+++ public/views/apps/layout/cetak/data.php
@@ -15,18 +15,18 @@
             <div class="col-md-12">
               <div class="box box-solid">
                   <div class="box-header with-border">
-                      <h3 class="box-title"><i class="fa fa-files-o"></i> Cetak Data PPDB</h3>
+                      <h3 class="box-title"><i class="fa fa-print"></i> Cetak Data Pendaftar</h3>
                   </div>
                   <!-- /.box-header -->
                   <div class="box-body">
                     <div class="card-content">
                         <div class="main-menu-panduan" style="font-family: Roboto;font-weight: 300;font-size: 18px;text-decoration: none">
-                            <a href="<?php echo base_url() ?>apps/cetak/data_valid" style="text-decoration: none">
-                                <i class="fa fa-file-excel-o"></i>  Data Pendaftar Valid
+                            <a href="<?php echo base_url() ?>apps/cetak/data_valid" style="text-decoration: none" class="btn btn-success btn-save btn-fill">
+                                <i class="fa fa-print"></i>  Cetak Semua Data Pendaftar
                             </a>
                           <hr>
-                            <a href="<?php echo base_url() ?>apps/cetak/data_invalid" style="text-decoration: none">
-                                <i class="fa fa-file-excel-o"></i>  Data Pendaftar Invalid
+                            <a href="<?php echo base_url() ?>apps/cetak/data_invalid" style="text-decoration: none" class="btn btn-success btn-save btn-fill">
+                                <i class="fa fa-print"></i>  Cetak Data Pendaftar Yang Valid
                             </a>
                         </div>
                     </div>
