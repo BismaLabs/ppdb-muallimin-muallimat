7fc51508b02d68e851b7b72316e42c05c1b3f31a
maulayyacyber <maulayyacyber@gmail.com>
2017-03-23 06:16:29 +0700

    add visite site and forgot password template

--- public/views/apps/layout/auth/forgot.php
+++ public/views/apps/layout/auth/forgot.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Login - <?php echo systems('admin_title') ?></title>
+    <title>Forgot Password - Medical Top Team Administrator System</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link rel="stylesheet" href="<?php echo base_url() ?>resources/backend/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>resources/backend/font-awesome/css/font-awesome.min.css">
@@ -18,39 +18,24 @@
     <div class="login-box-body" style="-moz-box-shadow: 1px 1px 2px 0 rgba(0,0,0,.12);webkit-box-shadow: 1px 1px 2px 0 rgba(0,0,0,.12);box-shadow: 1px 1px 2px 0 rgba(0,0,0,.12);">
         <p class="login-box-msg">Administrator system</p>
         <?php
-            $attributes = array('id' => 'frm_login');
-            echo form_open('apps/login?source=login&utf8=✓', $attributes)
+        $attributes = array('id' => 'frm_login');
+        echo form_open('apps/login/forgot?source=login&utf8=✓', $attributes)
         ?>
         <div class="form-group has-feedback">
-            <input type="text" class="form-control" name="username" placeholder="Username">
-            <span class="glyphicon glyphicon-user form-control-feedback"></span>
-            <?php echo form_error('username'); ?>
+            <input type="email" class="form-control" name="email" placeholder="Email Address">
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            <?php echo form_error('email'); ?>
         </div>
-        <div class="form-group has-feedback">
-            <input type="password" class="form-control" name="password" placeholder="Password">
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-            <?php echo form_error('password'); ?>
-        </div>
-        <?php if(isset($error)) { echo $error; }; ?>
+        <?php echo $this->session->flashdata('notif'); ?>
         <div class="row">
-            <div class="col-xs-8">
-                <div class="checkbox icheck">
-                    <label>
-                        <input type="checkbox"> Remember Me
-                    </label>
-                </div>
+            <div class="col-xs-6">
+                <a href="<?php echo base_url() ?>apps/login/?source=forgot&utf8=✓" type="submit" class="btn btn-primary btn-block btn-flat" style="padding: 6px 1px;"><i class="fa fa-arrow-circle-left"></i> Back to login  </a>
             </div>
-            <div class="col-xs-4">
-                <button type="submit" class="btn bg-olive btn-block btn-flat">Login <i class="fa fa-unlock"></i> </button>
+            <div class="col-xs-6">
+                <button type="submit" class="btn bg-olive btn-block btn-flat" style="padding: 6px 1px;">Get new password <i class="fa fa-sign-out"></i> </button>
             </div>
-
         </div>
         <?php echo form_close(); ?>
-        <hr>
-        <div class="social-auth-links text-center">
-            <p> Forgot Password ? </p>
-            <a href="<?php echo base_url() ?>apps/login/forgot?source=login&utf8=✓" class="btn bg-orange btn-flat" style="text-align: center;width: 100%"><i class="fa fa-envelope"></i> Get new password</a>
-        </div>
     </div>
     <div class="text-center" style="margin-top: 10px;font-size: 15px;padding: 6px 6px">
         <?php echo systems('admin_footer') ?>
--- public/views/apps/part/header.php
+++ public/views/apps/part/header.php
@@ -33,6 +33,13 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </a>
+            <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="<?php echo base_url() ?>" target="_blank">Visit Site <i class="fa fa-sign-out"></i></a>
+                </li>
+            </ul>
+            </div>
 
             <div class="navbar-custom-menu">
                 <ul class="nav navbar-nav">
@@ -65,7 +72,7 @@
                                     <a href="<?php echo base_url() ?>apps/users/edit/<?php echo $this->encryption->encode($this->session->userdata('apps_id')) ?>/" class="btn btn-default btn-flat"><i class="fa fa-user-circle-o"></i> Profile</a>
                                 </div>
                                 <div class="pull-right">
-                                    <a href="<?php echo base_url() ?>apps/dashboard/logout/" class="btn btn-default btn-flat"> Logout <i class="fa fa-sign-out"></i></a>
+                                    <a href="<?php echo base_url() ?>apps/dashboard/logout/" class="btn btn-default btn-flat"> Logout <i class="fa fa-ban"></i></a>
                                 </div>
                             </li>
                         </ul>
