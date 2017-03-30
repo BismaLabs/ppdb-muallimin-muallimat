3815d7ac5307acfb0872377b959e6c88d85ad180
maulayyacyber <maulayyacyber@gmail.com>
2017-03-30 21:36:36 +0700

    add pdf config

--- public/views/apps/layout/ppdb/data.php
+++ public/views/apps/layout/ppdb/data.php
@@ -1,4 +1,3 @@
-
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -20,12 +19,16 @@
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
-                        <form method="GET" action="<?php echo base_url('apps/ppdb/search');?>" style="margin-top: 10px">
-                            <div class = "input-group">
-                                <input type = "text" name = "q" class = "form-control input-md" placeholder="Masukkan Kode Pendaftaran atau Nama  dan Enter" autocomplete="off" id="articles">
-                                <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
-                                <span class = "input-group-btn">
-                              <button class = "btn btn-default btn-md" type = "submit">
+                        <form method="GET" action="<?php echo base_url('apps/ppdb/search'); ?>"
+                              style="margin-top: 10px">
+                            <div class="input-group">
+                                <input type="text" name="q" class="form-control input-md"
+                                       placeholder="Masukkan Kode Pendaftaran atau Nama  dan Enter" autocomplete="off"
+                                       id="articles">
+                                <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>"
+                                       value="<?php echo $this->security->get_csrf_hash(); ?>">
+                                <span class="input-group-btn">
+                              <button class="btn btn-default btn-md" type="submit">
                                  <i class="fa fa-search"></i> Search
                               </button>
                            </span>
@@ -33,60 +36,78 @@
                         </form>
                         <!-- Table Data -->
                         <div class="table-responsive">
-                          <table class="table table-bordered table-striped table-hover" style="margin-top:20px;font-family: Roboto;font-weight: 300;">
-                            <thead>
-                              <tr>
-                                <th class="text-center" style="color: #000;"><i class="fa fa-list-ul"></i> NO. PENDAFTARAN</th>
-                                <th class="text-center" style="color: #000;"><i class="fa fa-user-circle"></i> NAMA LENGKAP</th>
-                                <th class="text-center" style="color: #000;"><i class="fa fa-building-o"></i> KELAS</th>
-                                <th class="text-center" style="color: #000;"><i class="fa fa-mars"></i> JENIS KELAMIN</th>
-                                <th class="text-center" style="color: #000;"><i class="fa fa-info-circle"></i> STATUS</th>
-                                  <th class="text-center" style="color: #000;"><i class="fa fa-print"></i> CETAK</th>
-                                <th class="text-center" style="color: #000;"><i class="fa fa-cogs"></i> OPTIONS</th>
-                              </tr>
-                            </thead>
-                              <?php
-                              if($ppdb != NULL):
-                              foreach($ppdb->result() as $hasil):
+                            <table class="table table-bordered table-striped table-hover"
+                                   style="margin-top:20px;font-family: Roboto;font-weight: 300;">
+                                <thead>
+                                <tr>
+                                    <th class="text-center" style="color: #000;"><i class="fa fa-list-ul"></i> NO.
+                                        PENDAFTARAN
+                                    </th>
+                                    <th class="text-center" style="color: #000;"><i class="fa fa-user-circle"></i> NAMA
+                                        LENGKAP
+                                    </th>
+                                    <th class="text-center" style="color: #000;"><i class="fa fa-building-o"></i> KELAS
+                                    </th>
+                                    <th class="text-center" style="color: #000;"><i class="fa fa-mars"></i> JENIS
+                                        KELAMIN
+                                    </th>
+                                    <th class="text-center" style="color: #000;"><i class="fa fa-info-circle"></i>
+                                        STATUS
+                                    </th>
+                                    <th class="text-center" style="color: #000;"><i class="fa fa-print"></i> CETAK</th>
+                                    <th class="text-center" style="color: #000;"><i class="fa fa-cogs"></i> OPTIONS</th>
+                                </tr>
+                                </thead>
+                                <?php
+                                if ($ppdb != NULL):
+                                foreach ($ppdb->result() as $hasil):
 
-                              if($hasil->status == "0"){
+                                    if ($hasil->status == "0") {
 
-                                  $status = '<span class="badge badge-danger" style="font-family: Roboto;font-weight: 400;background-color: #ff8412;"><i class="fa fa-circle-o-notch fa-spin"></i> Pending</span>';
+                                        $status = '<span class="badge badge-danger" style="font-family: Roboto;font-weight: 400;background-color: #ff8412;"><i class="fa fa-circle-o-notch fa-spin"></i> Pending</span>';
 
-                                  $update_status = '<a class="badge badge-primary" style="font-family: Roboto;font-weight: 400;background-color: #1969bc;" data-toggle="tooltip" data-placement="top" title="Yakin menerima ?" href="'.base_url().'apps/ppdb/confirm/'.$this->encryption->encode($hasil->kode_pendaftaran).'/'.$this->encryption->encode('1').'"><i class="fa fa-check-circle"></i> Update</a>';
+                                        $update_status = '<a class="badge badge-primary" style="font-family: Roboto;font-weight: 400;background-color: #1969bc;" data-toggle="tooltip" data-placement="top" title="Ingin Memvalidasi ?" href="' . base_url() . 'apps/ppdb/confirm/' . $this->encryption->encode($hasil->kode_pendaftaran) . '/' . $this->encryption->encode('1') . '"><i class="fa fa-check-circle"></i> Update</a>';
 
-                              }elseif($hasil->status == "1"){
+                                    } elseif ($hasil->status == "1") {
 
-                                  $status = '<span class="badge badge-success" style="font-family: Roboto;font-weight: 400;background-color: #358420;"><i class="fa fa-check-circle"></i> Ditermia</span>';
+                                        $status = '<span class="badge badge-success" style="font-family: Roboto;font-weight: 400;background-color: #358420;"><i class="fa fa-check-circle"></i> Validated</span>';
 
-                                  $update_status = '<a class="badge badge-primary" style="font-family: Roboto;font-weight: 400;background-color: #1969bc;" data-toggle="tooltip" data-placement="top" title="Batal menerima ?" href="'.base_url().'apps/ppdb/confirm/'.$this->encryption->encode($hasil->kode_pendaftaran).'/'.$this->encryption->encode('0').'"><i class="fa fa-ban"></i> Update</a>';
-                              }
+                                        $update_status = '<a class="badge badge-primary" style="font-family: Roboto;font-weight: 400;background-color: #1969bc;" data-toggle="tooltip" data-placement="top" title="Batal Validasi ?" href="' . base_url() . 'apps/ppdb/confirm/' . $this->encryption->encode($hasil->kode_pendaftaran) . '/' . $this->encryption->encode('0') . '"><i class="fa fa-ban"></i> Update</a>';
+                                    }
 
-                              ?>
+                                    ?>
 
-                              <tr>
-                                <td><?php echo $hasil->kode_pendaftaran ?></td>
-                                <td><?php echo $hasil->nama_lengkap ?></td>
-                                <td><?php echo $hasil->pendaftaran_kelas ?></td>
-                                <td><?php echo $hasil->jenis_kelamin ?></td>
-                                <td style="text-align: center">
-                                    <?php echo $status ?>
-                                </td>
-                                  <td style="text-align: center">
-                                      <a class="badge badge-success" style="font-family: Roboto;font-weight: 400;background-color: #841d05;" data-toggle="tooltip" data-placement="top" title="Cetak Formulir" href="#"><i class="fa fa-print"></i> Formulir</a>
-                                      <a class="badge badge-success" style="font-family: Roboto;font-weight: 400;background-color: #841d05;" data-toggle="tooltip" data-placement="top" title="Cetak Kartu Ujian" href="#"><i class="fa fa-print"></i> Kartu Ujian</a>
-                                  </td>
-                                  <td>
-                                  <div class="btn-group pull-right" role="group" >
-                                      <?php echo $update_status ?>
-                                  </div>
-                                 </td>
-                              </tr>
-                              <?php
-                              endforeach;
-                              ?>
-                            </tbody>
-                          </table>
+                                    <tr>
+                                        <td><?php echo $hasil->kode_pendaftaran ?></td>
+                                        <td><?php echo $hasil->nama_lengkap ?></td>
+                                        <td><?php echo $hasil->pendaftaran_kelas ?></td>
+                                        <td><?php echo $hasil->jenis_kelamin ?></td>
+                                        <td style="text-align: center">
+                                            <?php echo $status ?>
+                                        </td>
+                                        <td style="text-align: center">
+                                            <a class="badge badge-success"
+                                               style="font-family: Roboto;font-weight: 400;background-color: #841d05;"
+                                               data-toggle="tooltip" data-placement="top" title="Cetak Formulir"
+                                               href="<?php echo base_url() ?>apps/ppdb/cetak_formulir/<?php echo $this->encryption->encode($hasil->kode_pendaftaran) ?>/"><i
+                                                        class="fa fa-print"></i> Formulir</a>
+                                            <a class="badge badge-success"
+                                               style="font-family: Roboto;font-weight: 400;background-color: #841d05;"
+                                               data-toggle="tooltip" data-placement="top" title="Cetak Kartu Ujian"
+                                               href="<?php echo base_url() ?>apps/ppdb/cetak_kartu_ujian/<?php echo $this->encryption->encode($hasil->kode_pendaftaran) ?>/"><i
+                                                        class="fa fa-print"></i> Kartu Ujian</a>
+                                        </td>
+                                        <td>
+                                            <div class="btn-group pull-right" role="group">
+                                                <?php echo $update_status ?>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                endforeach;
+                                ?>
+                                </tbody>
+                            </table>
                             <?php echo $paging ?>
                             <?php else : ?>
                                 </tbody>
@@ -95,11 +116,14 @@
                                     <span><b> Warning! </b> Data tidak ada didatabase </span>
                                 </div>
                                 <div class="reload" style="text-align: center;margin-bottom: 7px">
-                                    <a  href="<?php echo base_url('apps/ppdb?source=reload&utf8=✓') ?>" class="btn btn-danger btn-reset btn-fill" id="load" data-loading-text="<i class='fa fa-circle-o-notch fa-spin'></i> Reloading..."><i class="fa fa-repeat"></i> Reload Page</a>
+                                    <a href="<?php echo base_url('apps/ppdb?source=reload&utf8=✓') ?>"
+                                       class="btn btn-danger btn-reset btn-fill" id="load"
+                                       data-loading-text="<i class='fa fa-circle-o-notch fa-spin'></i> Reloading..."><i
+                                                class="fa fa-repeat"></i> Reload Page</a>
                                 </div>
                             <?php endif; ?>
                             </tbody>
-                        </table>
+                            </table>
                         </div>
                     </div>
                     <!-- /.box-body -->
