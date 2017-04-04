48d641af55e14be8bea760a7fa4a62c9fd78fe42
maulayyacyber <maulayyacyber@gmail.com>
2017-04-04 09:52:31 +0700

    layout kartu ujian

---
+++ public/views/apps/layout/ppdb/cetak_kartu.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Cetak Kartu Peserta PPDB</title>
+
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url() ?>resources/public/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<table style="width: 50%;padding-top: 5%;padding-bottom: 5%;padding-left: 20%;padding: 15%" border="1">
+    <tr>
+        <td style="padding-right: 5%;">
+            <br>
+            PERHATIAN
+            <br>
+            <br>
+
+            <div style="padding-top: 20%;text-align: center;padding-left: 20%">
+                1. Peserta Ujian harus sudah ada di lokasi 07.30 dengan seragam Hitam - Putih;
+                <br>
+                <br>
+                2. Kartu Peserta ini jangan sampai hilang atau rusak sampai Peserta Daftar;
+                <br>
+                <br>
+                3. Dilarang membawa buku-buku atau catatan apapun selama Ujian, kecuali alat tulis menulis;
+            </div>
+            <br>
+            <br>
+        </td>
+
+    </tr>
+</table>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="<?php echo base_url() ?>resources/public/js/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?php echo base_url() ?>resources/public/js/bootstrap.min.js"></script>
+</body>
+</html>
